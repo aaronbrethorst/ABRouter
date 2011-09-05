3d723d3de08ad350775f0bde2fb1321ee630ce08
Aaron Brethorst <aaron@brethorsting.com>
2011-09-05 13:35:37 -0700

    A work of art this code is not, but add some more niceties from the (very nicely designed) Gowalla API.

--- GowallaExample/GowallaExample/View Controllers/SpotViewController.h
+++ GowallaExample/GowallaExample/View Controllers/SpotViewController.h
@@ -9,5 +9,15 @@
 #import <UIKit/UIKit.h>
 
 @interface SpotViewController : UIViewController <Routable>
+@property(nonatomic,retain) NSMutableArray *photoURLs;
+@property(nonatomic,retain) IBOutlet UIImageView *categoryImage;
+@property(nonatomic,retain) IBOutlet UILabel *businessName;
+@property(nonatomic,retain) IBOutlet UIImageView *photoOne;
+@property(nonatomic,retain) IBOutlet UIImageView *photoTwo;
+@property(nonatomic,retain) IBOutlet UIImageView *photoThree;
+@property(nonatomic,retain) IBOutlet UIImageView *photoFour;
+@property(nonatomic,retain) IBOutlet UIImageView *photoFive;
 @property(nonatomic,retain) NSString *apiPath;
+@property(nonatomic,retain) NSDictionary *spot;
+- (IBAction)viewCheckins:(id)sender;
 @end
